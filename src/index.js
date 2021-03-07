b6017727c547de8c5b37b8a75f6fac8d91bdb9ce
k-lenka <alena1027@hmail.com>
2021-03-07 13:18:26 -0400

    feat: tests solve

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return array?.length 
+    ? Math.min(...array)
+    : 0;
+
+    // if (!(array && array.length)) {
+    //     return 0;
+    // }
+
+    // let minNumber = array[0];
+    // for (let i = 1; i < array.length; i++) {
+    //     if (array[i] < minNumber) {
+    //         minNumber = array[i];
+    //     }
+    // }
+    // return minNumber;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+return array?.length
+? Math.max(...array)
+: 0;
+
+    // if (!(array && array.length)) {
+    //     return 0;
+    // }
+
+    // let maxNumber = array[0]
+    // for (let i = 1; i < array.length; i++) {
+    //     if (array[i] > maxNumber) {
+    //         maxNumber = array[i];
+    //     }
+    // }
+    // return maxNumber;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return array?.length 
+    ? array.reduce((acc, val) => acc + val) / array.length
+    : 0;
 }
+
+
