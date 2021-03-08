a7307bfc2d93b462d4645421880e7de712d06a79
k-lenka <alena1027@hmail.com>
2021-03-07 20:38:51 -0400

    refactor: change new features with old ones

--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
 
 exports.min = function min(array) {
-    return array?.length 
-    ? Math.min(...array)
-    : 0;
+    if (!array || !array.length){
+    return 0}
 
-    // if (!(array && array.length)) {
-    //     return 0;
-    // }
+       let minNumber = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minNumber) {
+            minNumber = array[i];
+        }
+    }
+return minNumber;
 
-    // let minNumber = array[0];
-    // for (let i = 1; i < array.length; i++) {
-    //     if (array[i] < minNumber) {
-    //         minNumber = array[i];
-    //     }
-    // }
-    // return minNumber;
 }
 
 exports.max = function max(array) {
-return array?.length
-? Math.max(...array)
-: 0;
+  if (!(array && array.length)) {
+        return 0;
+    }
 
-    // if (!(array && array.length)) {
-    //     return 0;
-    // }
-
-    // let maxNumber = array[0]
-    // for (let i = 1; i < array.length; i++) {
-    //     if (array[i] > maxNumber) {
-    //         maxNumber = array[i];
-    //     }
-    // }
-    // return maxNumber;
+    let maxNumber = array[0]
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxNumber) {
+            maxNumber = array[i];
+        }
+    }
+    return maxNumber;
 }
 
 exports.avg = function avg(array) {
-    return array?.length 
-    ? array.reduce((acc, val) => acc + val) / array.length
-    : 0;
+    if (!(array && array.length)) {
+        return 0;
+    }
+
+    let arrAvg = (acc, val) => acc + val;
+
+    return array.reduce(arrAvg) / array.length;
+    
 }
 
 
